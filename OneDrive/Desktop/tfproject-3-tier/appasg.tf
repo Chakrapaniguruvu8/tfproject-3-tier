cb91ec25dcbef5fa9d1a02655dd5c8ca40564c50
Chakrapani guruvu <163977881+Chakrapaniguruvu8@users.noreply.github.com>
2025-08-21 15:14:11 +0530

    Update appasg.tf

--- OneDrive/Desktop/tfproject-3-tier/appasg.tf
+++ OneDrive/Desktop/tfproject-3-tier/appasg.tf
@@ -1,6 +1,6 @@
 # Create an EC2 Auto Scaling Group - app
 resource "aws_autoscaling_group" "swiggy-app-asg" {
-  name = "swiggy-app-asg"
+  name = "swiggy-chakri-asg"
 
   launch_template {
     id      = aws_launch_template.swiggy-app-template.id
@@ -19,10 +19,10 @@ resource "aws_autoscaling_group" "swiggy-app-asg" {
 
 # Create a launch template for the EC2 instances
 resource "aws_launch_template" "swiggy-app-template" {
-  name_prefix   = "swiggy-app-template"
-  image_id      = "ami-0861f4e788f5069dd"
-  instance_type = "t2.micro"
-  key_name      = "3tierproject"
+  name_prefix   = "swiggy-chakri-template"
+  image_id      = "ami-0c4a668b99e68bbde"
+  instance_type = "t3.micro"
+  key_name      = "3tier"
 
   network_interfaces {
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg-app.id]
