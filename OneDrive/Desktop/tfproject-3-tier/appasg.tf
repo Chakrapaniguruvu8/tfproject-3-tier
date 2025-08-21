86908969e6d6e5818c515129ad77313adfcf76d1
Chakrapani guruvu <163977881+Chakrapaniguruvu8@users.noreply.github.com>
2025-08-21 18:24:55 +0530

    Update appasg.tf

--- OneDrive/Desktop/tfproject-3-tier/appasg.tf
+++ OneDrive/Desktop/tfproject-3-tier/appasg.tf
@@ -1,15 +1,15 @@
 # Create an EC2 Auto Scaling Group - app
-resource "aws_autoscaling_group" "swiggy-app-asg" {
+resource "aws_autoscaling_group" "swiggy_app_asg" {
   name = "swiggy-appch-asg"
 
   launch_template {
-    id      = aws_launch_template.swiggy-app-template.id
+    id      = aws_launch_template.swiggy_app_template.id
     version = "$Latest"
   }
 
   vpc_zone_identifier = [
-    aws_subnet.swiggy-pvt-sub-1.id,
-    aws_subnet.swiggy-pvt-sub-2.id
+    aws_subnet.swiggy_pvt_sub_1.id,
+    aws_subnet.swiggy_pvt_sub_2.id
   ]
 
   min_size         = 2
@@ -18,26 +18,27 @@ resource "aws_autoscaling_group" "swiggy-app-asg" {
 }
 
 # Create a launch template for the EC2 instances
-resource "aws_launch_template" "swiggy-app-template" {
+resource "aws_launch_template" "swiggy_app_template" {
   name_prefix   = "swiggy-appch-template"
   image_id      = "ami-0861f4e788f5069dd"
   instance_type = "t3.micro"
   key_name      = "3tierprojectforchakri"
 
   network_interfaces {
-    security_groups             = [aws_security_group.swiggy-ec2-asg-sg-app.id]
+    security_groups             = [aws_security_group.swiggy_ec2_asg_sg_app.id]
     associate_public_ip_address = false
   }
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
-
-    sudo yum install mysql -y
+    sudo yum update -y
+    sudo yum install -y mysql
   EOF
   )
 
   lifecycle {
     prevent_destroy = true
-    ignore_changes  = all
+    # if you want to ignore some attributes:
+    # ignore_changes = [tags, user_data]
   }
 }
