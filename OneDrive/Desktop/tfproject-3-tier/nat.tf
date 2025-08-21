a05fe6e6790018f74f43d39a180af2542642d91f
Chakrapani guruvu <163977881+Chakrapaniguruvu8@users.noreply.github.com>
2025-08-21 15:17:42 +0530

    Update nat.tf

--- OneDrive/Desktop/tfproject-3-tier/nat.tf
+++ OneDrive/Desktop/tfproject-3-tier/nat.tf
@@ -1,7 +1,7 @@
 #NatGW
 resource "aws_nat_gateway" "swiggy-natgw-01" {
   allocation_id = aws_eip.swiggy-nat-eip.id
-  subnet_id     = aws_subnet.swiggy-pub-sub-1.id
+  subnet_id     = aws_subnet.swiggy23-pub-sub-1.id
 
   tags = {
     Name = "swiggy-natgwchakri-1"
