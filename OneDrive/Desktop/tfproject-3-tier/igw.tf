e9ea9502afbfbe7703c6ac9ade1cb61b16b919d4
Chakrapani guruvu <163977881+Chakrapaniguruvu8@users.noreply.github.com>
2025-08-21 17:41:48 +0530

    Update igw.tf

--- OneDrive/Desktop/tfproject-3-tier/igw.tf
+++ OneDrive/Desktop/tfproject-3-tier/igw.tf
@@ -1,8 +1,7 @@
 # Internet Gateway
-resource "aws_internet_gateway" "swiggy-igw33" {
+resource "aws_internet_gateway" "swiggy-igw" {
   tags = {
-    Name = "swiggy-igw33"
+    Name = "swiggy-igw233"
   }
-  vpc_id = aws_vpc.swiggy23-vpc.id
+  vpc_id = aws_vpc.swiggy-vpc.id
 }
-
