ca097dfd10fdc67cd3a2fb04ad830ef6abda42d0
Chakrapani guruvu <163977881+Chakrapaniguruvu8@users.noreply.github.com>
2025-08-21 17:32:30 +0530

    Update subnet.tf

--- OneDrive/Desktop/tfproject-3-tier/subnet.tf
+++ OneDrive/Desktop/tfproject-3-tier/subnet.tf
@@ -1,62 +1,62 @@
 # Public Subnets 
-resource "aws_subnet" "swiggy-pub-sub-1" {
+resource "aws_subnet" "swiggy-pubc-sub-1" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.0/28"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = "true"
 
   tags = {
-    Name = "swiggy-pub-sub-1"
+    Name = "swiggy-pubc-sub-1"
   }
 }
 
-resource "aws_subnet" "swiggy-pub-sub-2" {
+resource "aws_subnet" "swiggy-pubch-sub-2" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.16/28"
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = "true"
   tags = {
-    Name = "swiggy-pub-sub-2"
+    Name = "swiggy-pubch-sub-2"
   }
 }
 
 
 # Private Subnets
-resource "aws_subnet" "swiggy-pvt-sub-1" {
+resource "aws_subnet" "swiggy-pvtc-sub-1" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.32/28"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = false
   tags = {
-    Name = "swiggy-pvt-sub-1"
+    Name = "swiggy-pvtc-sub-1"
   }
 }
-resource "aws_subnet" "swiggy-pvt-sub-2" {
+resource "aws_subnet" "swiggy-pvtch-sub-2" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.48/28"
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = false
   tags = {
-    Name = "swiggy-pvt-sub-2"
+    Name = "swiggy-pvtch-sub-2"
   }
 }
 
-resource "aws_subnet" "swiggy-pvt-sub-3" {
+resource "aws_subnet" "swiggy-pvtchh-sub-3" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.64/28"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = false
   tags = {
-    Name = "swiggy-pvt-sub-3"
+    Name = "swiggy-pvtchh-sub-3"
   }
 }
-resource "aws_subnet" "swiggy-pvt-sub-4" {
+resource "aws_subnet" "swiggy-pvtchch-sub-4" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.80/28"
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = false
   tags = {
-    Name = "swiggy-pvt-sub-4"
+    Name = "swiggy-pvtchch-sub-4"
   }
 }
 
